b1c114d3f5841502d83f9ee78555171d82577819
Yoon <i552267@c02fn51zmd6m.skbroadband>
2021-08-08 00:26:53 +0900

    initial commit

---
+++ html_writer.py
@@ -0,0 +1,51 @@
+import json
+import datetime
+
+
+class HTMLWriter:
+    def __init__(self):
+        self.filename = "data.json"
+
+    def initialize(self):
+        data = {"current_number": 0}
+        with open(self.filename, "w") as f:
+            json.dump(data, f)
+
+    def write(self):
+        with open(self.filename, "r") as f:
+            data = json.load(f)
+        data["current_number"] += 1
+        with open(self.filename, "w") as f:
+            json.dump({"current_number": data["current_number"]}, f)
+
+        data = f"""\
+<table cellspacing="0" cellpadding="0" border="1" style="width: 159px; height: 163px; font-size: 10pt; border-width: 0px; border-color: rgb(0, 0, 0); border-collapse: collapse; border-style: solid; background-color: rgb(255, 255, 255);" class="">
+<tbody>
+<tr>
+<td style="width: 158px; height: 162px; border-width: 1px; border-color: rgb(0, 0, 0); border-style: solid;">
+<p style="font-size: 13.3333px; text-align: center; line-height: 160%;"><br></p>
+<p style="font-size: 13.3333px; text-align: center; line-height: 160%;">발권시간 : {self.current_time}</p>
+<p style="font-size: 13.3333px; text-align: center; line-height: 160%;"><br></p>
+<p style="font-size: 13.3333px; text-align: center; line-height: 160%;"><span style="font-size: 22pt; font-weight: bold;">순 번 표</span></p>
+<p style="font-size: 13.3333px; text-align: center; line-height: 160%;"><br></p>
+<p style="font-size: 13.3333px; text-align: center; line-height: 160%;">대 기 번 호</p>
+<p style="font-size: 13.3333px; text-align: center; line-height: 160%;"><span style="font-size: 22pt;">{data["current_number"]}</span></p>
+<p style="font-size: 13.3333px; text-align: center; line-height: 160%;"><br></p>
+</td>
+</tr>
+</tbody>
+</table>
+<p style="line-height: 160%;"><br></p>"""
+
+        with open("index.html", "w") as file:
+            file.write(data)
+
+    @property
+    def current_time(self):
+        return datetime.datetime.now().strftime("%H시 %M분")
+
+
+if __name__ == "__main__":
+    html_writer = HTMLWriter()
+    html_writer.initialize()
+    html_writer.write()
---
+++ main.py
@@ -0,0 +1,88 @@
+import os
+import sys
+
+from PyQt5.QtWidgets import (
+    QApplication,
+    QWidget,
+    QPushButton,
+    QVBoxLayout,
+    QHBoxLayout,
+)
+from PyQt5.QtWebEngineWidgets import QWebEngineView
+from PyQt5.QtCore import Qt, QUrl, QEvent, QEventLoop
+from PyQt5.QtPrintSupport import QPrinter
+from PyQt5.QtGui import QPainter
+
+from html_writer import HTMLWriter
+
+QApplication.setAttribute(Qt.AA_EnableHighDpiScaling, True)
+
+
+class Form(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.html_writer = HTMLWriter()
+        self.html_writer.initialize()
+        self.html_writer.write()
+
+        index_path = "file://" + os.path.join(os.getcwd(), "index.html")
+        self.home = QUrl(index_path)
+
+        hbox = QHBoxLayout()
+        self.btn_prn = QPushButton('Print')
+        hbox.addWidget(self.btn_prn)
+
+        self.webview = QWebEngineView()
+        self.webview.load(self.home)
+
+        vbox = QVBoxLayout(self)
+        vbox.addLayout(hbox)
+        vbox.addWidget(self.webview)
+
+        self.setWindowTitle('순번표 출력기')
+        self.setLayout(vbox)
+
+        QApplication.instance().installEventFilter(self)
+        self.btn_prn.clicked.connect(self.onPrint)
+
+    def eventFilter(self, obj, e):
+        if e.type() == QEvent.KeyPress and e.key() == Qt.Key_Return:
+            self.onMove()
+            return True
+        return super().eventFilter(obj, e)
+
+    def onPrint(self):
+        printer = QPrinter()
+        printer.setResolution(QPrinter.HighResolution)
+        printer.setPageSize(QPrinter.A4)
+        loop = QEventLoop()
+        result = False
+
+        def printPreView(success):
+            nonlocal result
+            result = success
+            loop.quit()
+
+        page = self.webview.page()
+        page.print(printer, printPreView)
+        loop.exec_()
+
+        if not result:
+            qp = QPainter()
+            if qp.begin(printer):
+                font = qp.font()
+                font.setPixelSize(20)
+                qp.setFont(font)
+                qp.drawText(10, 25, "Could not generate print preview")
+                qp.end()
+
+        self.html_writer.write()
+        self.webview.load(self.home)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    w = Form()
+    w.show()
+    sys.exit(app.exec_())
